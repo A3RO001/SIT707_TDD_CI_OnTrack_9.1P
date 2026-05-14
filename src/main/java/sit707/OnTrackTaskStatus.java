6c14feb6b0c484093d2c726434f1c6011c9f1375
A3RO001 <s224726304@deakin.edu.au>
2026-05-14 18:26:49 +1000

    Add CI workflow with intentionally failing test

--- src/main/java/sit707/OnTrackTaskStatus.java
+++ src/main/java/sit707/OnTrackTaskStatus.java
@@ -13,7 +13,7 @@ public class OnTrackTaskStatus {
         }
 
         if (studentId.equals("224726304") && taskId.equals("7.1P")) {
-            return "Submitted";
+            return "Wrong Status";
         }
 
         if (studentId.equals("224726304") && taskId.equals("8.1P")) {
